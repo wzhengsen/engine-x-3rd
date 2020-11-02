32dba062f81bac889890fd254367d3fa3a76c0ff
wzhengsen <wzhengsen@gmail.com>
2020-11-02 20:34:27 +0800

    [fix]Some compile error after merged.

--- cjson/lua_cjson.h
+++ cjson/lua_cjson.h
@@ -5,8 +5,9 @@
 #include "lua.h"
 
 #define USE_INTERNAL_FPCONV
-
-int luaopen_cjson(lua_State *l);
-int luaopen_cjson_safe(lua_State *l);
+extern "C" {
+    int luaopen_cjson(lua_State* l);
+    int luaopen_cjson_safe(lua_State* l);
+}
 
 #endif // __LUA_CJSON_H_
